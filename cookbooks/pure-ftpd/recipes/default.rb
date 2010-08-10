af47a86ed8215a08141a37f3fdfd7d3d00602780
Andy Duncan <andyduncan@gmail.com>
2010-08-10 14:55:14 -0700

    pureftpd v1

---
+++ cookbooks/pure-ftpd/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: pure-ftpd
+# Recipe:: default
+#
+package "net-ftp/pure-ftpd" do
+  action :install
+end
+
+directory "/data/files" do
+  owner node[:owner_name]
+  group node[:owner_name]
+  mode 0755
+  recursive true
+end
+
+execute "ensure-pure-ftpd-is-running" do
+  command %Q{
+    /usr/bin/pure-ftpd
+  }
+  not_if "pgrep pure-ftpd"
+end
